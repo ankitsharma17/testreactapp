9c15c389760211bcbfb0b9d1fc6bfb5775f1b140
Ankit Sharma <31661404+ankitsharma17@users.noreply.github.com>
2020-04-12 20:31:48 +0530

    code

---
+++ precache-manifest.dc680c65d504ceb018ae575572e39866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ea186fd51063fa9394",
+    "url": "/static/css/main.17637251.chunk.css"
+  },
+  {
+    "revision": "86ea186fd51063fa9394",
+    "url": "/static/js/main.c57b1d23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc286bc4200a41c081e5",
+    "url": "/static/js/2.4ea94689.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b920fd807cff905929a5fc6bd82d61d1",
+    "url": "/index.html"
+  }
+];
